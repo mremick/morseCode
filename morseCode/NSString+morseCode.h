7f5ba7a5d32d72b54f9d2dd818ab87afbd6e896b
Matt Remick <mremick@gmail.com>
2014-01-20 15:28:31 -0800

    initial commit

---
+++ morseCode/NSString+morseCode.h
@@ -0,0 +1,13 @@
+//
+//  NSString+morseCode.h
+//  morseCode
+//
+//  Created by Matt Remick on 1/20/14.
+//  Copyright (c) 2014 Matt Remick. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (morseCode)
+
+@end
---
+++ morseCode/NSString+morseCode.m
@@ -0,0 +1,13 @@
+//
+//  NSString+morseCode.m
+//  morseCode
+//
+//  Created by Matt Remick on 1/20/14.
+//  Copyright (c) 2014 Matt Remick. All rights reserved.
+//
+
+#import "NSString+morseCode.h"
+
+@implementation NSString (morseCode)
+
+@end
